51378252e0f7bf08f8e9e0a185ea364350096140
reisenbest <drgn96rd@gmail.com>
2024-04-15 01:15:41 +0300

    Update views.py

--- app/adminpanel/views.py
+++ app/adminpanel/views.py
@@ -1,8 +1,6 @@
-
-from django.shortcuts import render
+from rest_framework.permissions import IsAdminUser
 from service.models import Color, Palette
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.views import APIView
 from .serializers import PaletteAdminSerializer, ColorAdminSerializer, UserAdminSerializer
 from django.contrib.auth import get_user_model
 from drf_spectacular.utils import extend_schema
@@ -16,6 +14,7 @@ UserModel = get_user_model()
                tags=['Админ-палитры'])
 class PaletteAdminAPIViewset(ModelViewSet):
     queryset = Palette.objects.all().select_related('user')
+    permission_classes = (IsAdminUser,)
     serializer_class = PaletteAdminSerializer
     lookup_field = 'pk'
 
@@ -23,6 +22,7 @@ class PaletteAdminAPIViewset(ModelViewSet):
 @extend_schema(summary='Админ-панель для манипуляций с цветами',
                tags=['Админ-цвета'])
 class ColorAdminAPIViewset(ModelViewSet):
+    permission_classes = (IsAdminUser,)
     queryset = Color.objects.all().select_related('palette__user')
     serializer_class = ColorAdminSerializer
     lookup_field = 'pk'
@@ -31,8 +31,7 @@ class ColorAdminAPIViewset(ModelViewSet):
 @extend_schema(summary='Админ-панель для манипуляций с юзерами',
                tags=['Админ-юзеры'])
 class UserAdminAPIViewset(ModelViewSet):
+    permission_classes = (IsAdminUser,)
     queryset = UserModel.objects.all()
     serializer_class = UserAdminSerializer
     lookup_field = 'pk'
-
-
